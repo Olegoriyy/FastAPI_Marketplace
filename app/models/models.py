b6f5b79caf9c0f53953cfc501289528b0adfdb48
Oleg <taranenko.oleg@icloud.com>
2026-03-03 13:36:27 +0300

    initial project

---
+++ app/models/models.py
@@ -0,0 +1,18 @@
+from typing import Annotated
+
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.models.base import Base
+
+pk_int = Annotated[int, mapped_column(primary_key=True)]
+
+
+class Users(Base):
+    __tablename__ = 'users'
+
+    id: Mapped[pk_int]
+    username: Mapped[str] = mapped_column(String(128), unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(
+        String(320), unique=True, nullable=False, index=True
+    )
---
+++ app/schemas/user.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel, ConfigDict, EmailStr, Field
+
+
+class UserCreate(BaseModel):
+    username: str = Field(max_length=128)
+    password: str = Field(min_length=8, max_length=128)
+    email: EmailStr
+
+
+class UserPablic(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: int
+    username: str
+    email: EmailStr
